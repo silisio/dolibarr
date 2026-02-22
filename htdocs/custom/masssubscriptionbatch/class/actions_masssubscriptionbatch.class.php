f03a42c9d3b611d6b53d40b1ccf53ac5e24ce525
silisio <leo.brandenberger@gmail.com>
2026-02-22 02:42:06 +0100

    Add optional selected-members PDF export mass action

--- htdocs/custom/masssubscriptionbatch/class/actions_masssubscriptionbatch.class.php
+++ htdocs/custom/masssubscriptionbatch/class/actions_masssubscriptionbatch.class.php
@@ -4,6 +4,7 @@
 require_once DOL_DOCUMENT_ROOT.'/core/class/commonhookactions.class.php';
 require_once DOL_DOCUMENT_ROOT.'/adherents/class/adherent.class.php';
 require_once DOL_DOCUMENT_ROOT.'/adherents/class/adherent_type.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formmail.class.php';
 
 class ActionsMassSubscriptionBatch extends CommonHookActions
 {
@@ -20,7 +21,7 @@ class ActionsMassSubscriptionBatch extends CommonHookActions
 
 	public function addMoreMassActions($parameters, &$object, &$action, $hookmanager)
 	{
-		global $langs, $user;
+		global $conf, $langs, $massaction, $user;
 
 		$context = isset($parameters['currentcontext']) ? $parameters['currentcontext'] : (isset($parameters['context']) ? $parameters['context'] : '');
 		if (!in_array('memberlist', explode(':', (string) $context))) {
@@ -32,7 +33,188 @@ class ActionsMassSubscriptionBatch extends CommonHookActions
 
 		$langs->load('masssubscriptionbatch@masssubscriptionbatch');
 		$this->resprints = '<option value="masssubinvoiceemail">'.img_picto('', 'payment', 'class="pictofixedwidth"').$langs->trans('MassSubInvoiceEmailAction').'</option>';
+		if (getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_SETENDDATE')) {
+			$this->resprints .= '<option value="presetsubscriptionenddate">'.img_picto('', 'date', 'class="pictofixedwidth"').$langs->trans('MassSubSetEndDateAction').'</option>';
+		}
+		if (getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MASSMAIL')) {
+			$this->resprints .= '<option value="presend">'.img_picto('', 'email', 'class="pictofixedwidth"').$langs->trans('MassSubSendMailAction').'</option>';
+		}
+		if (getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MEMBERSPDF')) {
+			$this->resprints .= '<option value="preexportmemberspdf">'.img_picto('', 'pdf', 'class="pictofixedwidth"').$langs->trans('MassSubExportMembersPdfAction').'</option>';
+		}
 		return 0;
+	}
+
+
+
+	public function doPreMassActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $form, $langs, $user;
+
+		$massaction = $parameters['massaction'] ?? '';
+		if ($massaction === 'presend' && getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MASSMAIL')) {
+			$allowupload = (int) getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MASSMAIL_UPLOAD');
+			$note = json_encode($langs->transnoentitiesnoconv('MassSubSendMailNoMainDocNote'));
+			$addfilelabel = json_encode($langs->transnoentitiesnoconv('MailingAddFile'));
+			$uploadhint = json_encode($langs->transnoentitiesnoconv('MassSubSendMailUploadHint'));
+			$placeholdersTitle = json_encode($langs->transnoentitiesnoconv('MassSubSendMailPlaceholdersTitle'));
+			$placeholdersList = json_encode('__MEMBER_FULLNAME__, __MEMBER_FIRSTNAME__, __MEMBER_LASTNAME__, __MEMBER_EMAIL__, __MEMBER_ID__, __MEMBER_TYPE__, __MEMBER_LAST_SUBSCRIPTION_DATE_END__');
+			$this->resprints = '<script>document.addEventListener("DOMContentLoaded",function(){'
+				.'var cb=document.querySelector("input[name=\"addmaindocfile\"]");if(cb){cb.checked=false;cb.disabled=true;}'
+				.'var row=cb?(cb.closest(".tagtr")||cb.closest("tr")):null;'
+				.'var mailform=document.getElementById("mailform");if(mailform){mailform.enctype="multipart/form-data";mailform.encoding="multipart/form-data";}var target=row?((row.querySelector(".tagtd:last-child")||row.querySelector("td:last-child")||row)):mailform;'
+				.'var n=document.createElement("div");n.className="opacitymedium small";n.textContent='.$note.';if(target){target.appendChild(n);}'
+				.'if(target && '.$allowupload.'){var wrap=document.createElement("div");wrap.className="margintoponly";'
+					.'wrap.innerHTML="<input type=\"file\" class=\"flat\" id=\"addedfile\" name=\"addedfile\" form=\"mailform\"> '
+						.'<input type=\"submit\" class=\"button smallpaddingimp\" id=\"addfile\" name=\"addfile\" form=\"mailform\" formnovalidate value="+'.$addfilelabel.'+"/> '
+				.'<span class=\"opacitymedium\">"+'.$uploadhint.'+"</span>";target.appendChild(wrap);}'
+				.'var body=document.getElementById("message");if(body){var box=document.createElement("div");box.className="opacitymedium small margintop";'
+				.'box.innerHTML="<b>"+'.$placeholdersTitle.'+"</b><br><code>"+'.$placeholdersList.'+"</code>";body.parentNode.insertBefore(box, body);}'
+				.'});</script>';
+			return 0;
+		}
+
+		if ($massaction === 'preexportmemberspdf' && getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MEMBERSPDF')) {
+			$toselect = is_array($parameters['toselect'] ?? null) ? $parameters['toselect'] : array();
+			$fieldsHtml = ''
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="ref" checked> '.$langs->trans('MassSubPdfFieldRef').'</label><br>'
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="firstname" checked> '.$langs->trans('MassSubPdfFieldFirstName').'</label><br>'
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="lastname" checked> '.$langs->trans('MassSubPdfFieldLastName').'</label><br>'
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="email" checked> '.$langs->trans('MassSubPdfFieldEmail').'</label><br>'
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="type"> '.$langs->trans('MassSubPdfFieldType').'</label><br>'
+				.'<label><input type="checkbox" name="msb_pdf_fields[]" value="datefin"> '.$langs->trans('MassSubPdfFieldEndDate').'</label>';
+			$formquestion = array(array('type' => 'other', 'name' => 'msb_pdf_fields_block', 'label' => $langs->trans('MassSubExportMembersPdfFieldsLabel'), 'value' => $fieldsHtml));
+			$this->resprints = $form->formconfirm($_SERVER['PHP_SELF'], $langs->trans('MassSubExportMembersPdfTitle'), $langs->trans('MassSubExportMembersPdfQuestion', count($toselect)), 'exportmemberspdf', $formquestion, 1, 0, 260, 640, 1);
+			return 0;
+		}
+
+		if ($massaction !== 'presetsubscriptionenddate') {
+			return 0;
+		}
+		if (!$user->hasRight('adherent', 'creer') || !$user->hasRight('masssubscriptionbatch', 'run')) {
+			setEventMessages($langs->trans('NotEnoughPermissions'), null, 'errors');
+			return 0;
+		}
+
+		$toselect = is_array($parameters['toselect'] ?? null) ? $parameters['toselect'] : array();
+		$formquestion = array(
+			array('type' => 'date', 'name' => 'msb_enddate', 'label' => $langs->trans('MassSubSetEndDateField'), 'value' => dol_time_plus_duree(dol_now(), -1, 'd')),
+		);
+		$this->resprints = $form->formconfirm($_SERVER['PHP_SELF'], $langs->trans('MassSubSetEndDateConfirmTitle'), $langs->trans('MassSubSetEndDateConfirmQuestion', count($toselect)), 'setsubscriptionenddate', $formquestion, 1, 0, 200, 500, 1);
+
+		return 0;
+	}
+
+	public function doActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $langs, $massaction, $user;
+
+		if ($action === 'confirm_presend' && !getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MASSMAIL')) {
+			setEventMessages($langs->trans('MassSubSendMailDisabled'), null, 'errors');
+			$action = 'list';
+			return 1;
+		}
+		if ($action === 'confirm_presend' && getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MASSMAIL')) {
+			$_POST['addmaindocfile'] = 0;
+			$_REQUEST['addmaindocfile'] = 0;
+		}
+		if ($action === 'exportmemberspdf' && GETPOST('confirm', 'aZ09') === 'yes') {
+			if (!getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_MEMBERSPDF')) {
+				setEventMessages($langs->trans('MassSubExportMembersPdfDisabled'), null, 'errors');
+				return 1;
+			}
+			$fields = GETPOST('msb_pdf_fields', 'array');
+			if (!is_array($fields) || empty($fields)) {
+				$fields = array('ref', 'firstname', 'lastname', 'email');
+			}
+			$allowed = array('ref', 'firstname', 'lastname', 'email', 'type', 'datefin');
+			$fields = array_values(array_intersect($allowed, $fields));
+			$toselect = GETPOST('toselect', 'array');
+			if (!is_array($toselect) || empty($toselect)) {
+				setEventMessages($langs->trans('NoRecordSelected'), null, 'errors');
+				return 1;
+			}
+			require_once DOL_DOCUMENT_ROOT.'/core/lib/pdf.lib.php';
+			$pdf = pdf_getInstance('A4');
+			$pdf->SetCreator('Dolibarr');
+			$pdf->SetAuthor($user->getFullName($langs));
+			$pdf->SetTitle($langs->transnoentities('MassSubExportMembersPdfTitle'));
+			$pdf->SetMargins(10, 10, 10);
+			$pdf->AddPage();
+			$headers = array('ref' => $langs->transnoentities('Ref'), 'firstname' => $langs->transnoentities('Firstname'), 'lastname' => $langs->transnoentities('Lastname'), 'email' => $langs->transnoentities('Email'), 'type' => $langs->transnoentities('Type'), 'datefin' => $langs->transnoentities('DateEnd'));
+			$html = '<h2>'.$langs->transnoentities('MassSubExportMembersPdfTitle').'</h2><table border="1" cellpadding="3"><tr>';
+			foreach ($fields as $fieldkey) { $html .= '<th><b>'.dol_escape_htmltag((string) ($headers[$fieldkey] ?? $fieldkey)).'</b></th>'; }
+			$html .= '</tr>';
+			$member = new Adherent($this->db);
+			$typecache = array();
+			foreach ($toselect as $id) {
+				if ($member->fetch((int) $id) <= 0) continue;
+				if (!isset($typecache[(int) $member->typeid])) { $typeobj = new AdherentType($this->db); $typecache[(int) $member->typeid] = ($typeobj->fetch((int) $member->typeid) > 0) ? $typeobj->libelle : ''; }
+				$row = array('ref' => $member->ref, 'firstname' => $member->firstname, 'lastname' => $member->lastname, 'email' => $member->email, 'type' => $typecache[(int) $member->typeid], 'datefin' => $member->datefin ? dol_print_date($member->datefin, 'day') : '');
+				$html .= '<tr>';
+				foreach ($fields as $fieldkey) { $html .= '<td>'.dol_escape_htmltag((string) ($row[$fieldkey] ?? '')).'</td>'; }
+				$html .= '</tr>';
+			}
+			$html .= '</table>';
+			$pdf->writeHTML($html, true, false, true, false, '');
+			$pdfname = 'members_selection_'.dol_print_date(dol_now(), 'dayhourlog').'.pdf';
+			header('Content-Type: application/pdf');
+			header('Content-Disposition: attachment; filename="'.$pdfname.'"');
+			print $pdf->Output($pdfname, 'S');
+			exit;
+		}
+
+		if ($action !== 'setsubscriptionenddate' || GETPOST('confirm', 'aZ09') !== 'yes') {
+			return 0;
+		}
+		if (!getDolGlobalInt('MASSSUBSCRIPTIONBATCH_ENABLE_SETENDDATE')) {
+			setEventMessages($langs->trans('MassSubSetEndDateDisabled'), null, 'errors');
+			return 1;
+		}
+		if (!$user->hasRight('adherent', 'creer') || !$user->hasRight('masssubscriptionbatch', 'run')) {
+			setEventMessages($langs->trans('NotEnoughPermissions'), null, 'errors');
+			return 1;
+		}
+
+		$day = GETPOSTINT('msb_enddateday');
+		$month = GETPOSTINT('msb_enddatemonth');
+		$year = GETPOSTINT('msb_enddateyear');
+		$dateend = dol_mktime(0, 0, 0, $month, $day, $year);
+		if (empty($dateend)) {
+			setEventMessages($langs->trans('MassSubSetEndDateInvalid'), null, 'errors');
+			return 1;
+		}
+
+		$toselect = GETPOST('toselect', 'array');
+		$toselect = is_array($toselect) ? $toselect : array();
+
+		$member = new Adherent($this->db);
+		$nbupdated = 0;
+		$nberrors = 0;
+		$this->db->begin();
+		foreach ($toselect as $id) {
+			if ($member->fetch((int) $id) <= 0) {
+				$nberrors++;
+				continue;
+			}
+			$res = $member->setValueFrom('datefin', dol_print_date($dateend, '%Y-%m-%d'), '', null, 'date');
+			if ($res > 0) {
+				$nbupdated++;
+			} else {
+				$nberrors++;
+			}
+		}
+
+		if ($nberrors > 0) {
+			$this->db->rollback();
+			setEventMessages($langs->trans('MassSubSetEndDateErrors', $nberrors), null, 'errors');
+		} else {
+			$this->db->commit();
+			setEventMessages($langs->trans('MassSubSetEndDateDone', $nbupdated, dol_print_date($dateend, 'day')), null, 'mesgs');
+		}
+
+		$action = 'list';
+		return 1;
 	}
 
 	public function doMassActions($parameters, &$object, &$action, $hookmanager)
@@ -51,12 +233,31 @@ class ActionsMassSubscriptionBatch extends CommonHookActions
 		$langs->loadLangs(array('members', 'masssubscriptionbatch@masssubscriptionbatch'));
 		$toselect = is_array($parameters['toselect']) ? $parameters['toselect'] : array();
 		$sendmailenabled = (int) getDolGlobalInt('MASSSUBSCRIPTIONBATCH_DEFAULT_SENDMAIL');
+		if (empty($sendmailenabled)) {
+			setEventMessages($langs->trans('MassSubInvoiceEmailDisabled'), null, 'warnings');
+		}
+		$mailfrom = getDolGlobalString('ADHERENT_MAIL_FROM', $conf->email_from);
+		if ($sendmailenabled && empty($mailfrom)) {
+			$sendmailenabled = 0;
+			setEventMessages($langs->trans('MassSubInvoiceEmailSenderMissing'), null, 'errors');
+		}
+		$emailmaxperrun = max(0, (int) getDolGlobalInt('MASSSUBSCRIPTIONBATCH_EMAILS_PER_RUN', 100));
+		$emaildelayms = max(0, (int) getDolGlobalInt('MASSSUBSCRIPTIONBATCH_EMAIL_DELAY_MS', 200));
+		if ($sendmailenabled && $emailmaxperrun > 0 && count($toselect) > $emailmaxperrun) {
+			setEventMessages($langs->trans('MassSubInvoiceEmailLimitExceededAbort', count($toselect), $emailmaxperrun), null, 'errors');
+			return 0;
+		}
+		$templatelabel = getDolGlobalString('ADHERENT_EMAIL_TEMPLATE_SUBSCRIPTION');
+		$formmail = new FormMail($this->db);
+		$templatecache = array();
 
 		$member = new Adherent($this->db);
 		$membertype = new AdherentType($this->db);
 
 		$nbcreated = 0;
 		$nbsentemail = 0;
+		$nbemailfailed = 0;
+		$nbemailmissing = 0;
 		$nberrors = 0;
 		$datesubscription = dol_now();
 
@@ -117,16 +318,57 @@ class ActionsMassSubscriptionBatch extends CommonHookActions
 
 				$subject = $langs->transnoentitiesnoconv('MembershipPaid', dol_print_date($datesubend, 'day'));
 				$text = $membertype->getMailOnSubscription();
+
+				$langcode = empty($member->default_lang) ? $langs->defaultlang : $member->default_lang;
+				if (empty($templatecache[$langcode])) {
+					$templatecache[$langcode] = array('subject' => '', 'content' => '');
+					if (!empty($templatelabel)) {
+						$outputlangs = new Translate('', $conf);
+						$outputlangs->setDefaultLang($langcode);
+						$outputlangs->loadLangs(array('main', 'members'));
+						$arraydefaultmessage = $formmail->getEMailTemplate($this->db, 'member', $user, $outputlangs, 0, 1, $templatelabel);
+						if (is_object($arraydefaultmessage) && $arraydefaultmessage->id > 0) {
+							$templatecache[$langcode]['subject'] = (string) $arraydefaultmessage->topic;
+							$templatecache[$langcode]['content'] = (string) $arraydefaultmessage->content;
+						}
+					}
+				}
+
+				$substitutionarray = getCommonSubstitutionArray($langs, 0, null, $member);
+				complete_substitutions_array($substitutionarray, $langs, $member);
+				if (!empty($templatecache[$langcode]['subject'])) {
+					$subject = make_substitutions($templatecache[$langcode]['subject'], $substitutionarray, $langs);
+				}
+				if (!empty($templatecache[$langcode]['content'])) {
+					$text = make_substitutions(dol_concatdesc($templatecache[$langcode]['content'], $membertype->getMailOnSubscription()), $substitutionarray, $langs);
+				}
+
 				$ressend = $member->sendEmail($text, $subject, $listofpaths, $listofmimes, $listofnames);
 				if ($ressend > 0) {
 					$nbsentemail++;
+				} else {
+					$nbemailfailed++;
+					$nberrors++;
+					setEventMessages($langs->trans('MassSubInvoiceEmailSendFailed', $member->email), array($member->error), 'warnings');
 				}
+				if ($emaildelayms > 0) {
+					usleep($emaildelayms * 1000);
+				}
+			} elseif ($sendmailenabled) {
+				$nbemailmissing++;
+				setEventMessages($langs->trans('MassSubInvoiceEmailNoRecipient', $member->ref), null, 'warnings');
 			}
 		}
 
 		setEventMessages($langs->trans('XSubsriptionCreated', $nbcreated), null, 'mesgs');
 		if ($sendmailenabled) {
 			setEventMessages($langs->trans('XEmailSent', $nbsentemail), null, 'mesgs');
+			if ($nbemailfailed > 0) {
+				setEventMessages($langs->trans('MassSubInvoiceEmailFailedCount', $nbemailfailed), null, 'warnings');
+			}
+			if ($nbemailmissing > 0) {
+				setEventMessages($langs->trans('MassSubInvoiceEmailMissingCount', $nbemailmissing), null, 'warnings');
+			}
 		}
 		if ($nberrors > 0) {
 			setEventMessages($langs->trans('MassSubInvoiceEmailErrors', $nberrors), null, 'warnings');
